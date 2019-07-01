fd44b3a50937c4d3545e38ab30975664e9e77438
Dhanilan <mail2dhanilan@gmail.com>
2019-07-01 18:00:51 +0530

    End to End Test cases for scenarios in the spec

--- OnTheBeach/JobSequencing.Tests/EndToEndTests/CodingQuestions.cs
+++ OnTheBeach/JobSequencing.Tests/EndToEndTests/CodingQuestions.cs
@@ -8,8 +8,56 @@ namespace JobSequencing.Tests.EndToEndTests
     [TestClass]
     public class CodingQuestions
     {
+       
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod2()
+        {
+            var allLines = @"a =>
+                            b => 
+                            c =>";
+            var parser = ParserFactory.Create();
+            IJobTree tree = new JobTree();
+
+            foreach (var item in allLines.Split('\n'))
+            {
+                tree.Add(parser.Parse(item));
+            }
+
+            var result = tree.GetJobs();
+
+            Assert.IsTrue(result.Contains("a"));
+            Assert.IsTrue(result.Contains("b"));
+            Assert.IsTrue(result.Contains("c"));
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var allLines = @"a =>
+                            b => c
+                            c =>";
+            var parser = ParserFactory.Create();
+            IJobTree tree = new JobTree();
+
+            foreach (var item in allLines.Split('\n'))
+            {
+                tree.Add(parser.Parse(item));
+            }
+
+            var result = tree.GetJobs();
+
+            Assert.IsTrue(result.Contains("a"));
+            Assert.IsTrue(result.Contains("b"));
+            Assert.IsTrue(result.Contains("c"));
+            Assert.IsTrue(result.IndexOf("c") < result.IndexOf("b"));
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
         {
             var allLines = new List<string>();
             allLines.Add("a =>");
@@ -21,7 +69,7 @@ namespace JobSequencing.Tests.EndToEndTests
 
             var parser = ParserFactory.Create();
             IJobTree tree = new JobTree();
-            
+
             foreach (var item in allLines)
             {
                 tree.Add(parser.Parse(item));
@@ -36,11 +84,33 @@ namespace JobSequencing.Tests.EndToEndTests
         }
 
         [TestMethod]
-        public void TestMethod2()
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod5()
         {
             var allLines = @"a =>
-b => c
-c =>";
+                            b => c
+                            c => c";
+            var parser = ParserFactory.Create();
+            IJobTree tree = new JobTree();
+
+            foreach (var item in allLines.Split('\n'))
+            {
+                tree.Add(parser.Parse(item));
+            }           
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod6()
+        {
+            var allLines = @"a =>
+                            b => c
+                            c => f
+                            d => a
+                            e =>
+                            f => b";
             var parser = ParserFactory.Create();
             IJobTree tree = new JobTree();
 
@@ -49,12 +119,7 @@ c =>";
                 tree.Add(parser.Parse(item));
             }
 
-            var result = tree.GetJobs();
-            foreach (var item in result)
-            {
-                Console.Write(item);
-            }
-
         }
+
     }
 }

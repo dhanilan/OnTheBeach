bbb170eaa7fc2119d5cf956c30ee475c083496b9
Dhanilan <mail2dhanilan@gmail.com>
2019-07-01 11:21:09 +0530

    Job parsing with unit tests

--- JobSequencing/JobParser.cs
+++ JobSequencing/JobParser.cs
@@ -6,7 +6,20 @@ namespace JobSequencing
     {
         public Job Parse(string input)
         {
-            throw new NotImplementedException();
+            string[] args = input.Split(' ');
+
+            if (!(args.Length == 2 || args.Length == 3))
+                throw new ArgumentException("Input string in incorrect format");
+
+            if (args[1] != "=>")
+                throw new ArgumentException("implies operator expected");
+
+            var result = new Job(args[0]);
+
+            if (args.Length == 3)
+                result.ParentJobId = args[2];
+
+            return result;
         }
     }
 }
---
+++ OnTheBeach/JobSequencing.Tests/JobParser_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JobSequencing.Tests
+{
+    [TestClass]
+    public class JobParser_Tests
+    {
+        [TestMethod]
+        public void JobParser_ValidJobWithParent_ShouldParse()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A => B");
+            Assert.AreEqual(result.JobId,"A");
+            Assert.AreEqual(result.ParentJobId,"B");
+        }
+
+
+        [TestMethod]
+        public void JobParser_ValidJobWithoutParent_ShouldParse()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A =>");
+            Assert.AreEqual(result.JobId, "A");
+            Assert.IsNull(result.ParentJobId);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void JobParser_ValidJobWithEmpty_ShouldError()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("");           
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void JobParser_ValidJobWithMoreThanThreeParameters_ShouldError()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A B C B");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void JobParser_ValidJobWithoutImplies_ShouldError()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A = B");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void JobParser_ValidJobWithWrongSecondArgument_ShouldError()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A B");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void JobParser_ValidJobWithWrongNoImpliesArgument_ShouldError()
+        {
+            IJobParser parser = new JobParser();
+            var result = parser.Parse("A B C");
+        }
+
+    }
+}

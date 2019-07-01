46c162c70f1c8f5c52d10203531a1db53f8eaab0
Dhanilan <mail2dhanilan@gmail.com>
2019-07-01 15:13:03 +0530

    Refactor Folder structures

---
+++ JobSequencing/Parser/ParserFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobSequencing.Parser
+{
+    public class ParserFactory
+    {
+        static IJobParser Create()
+        {
+            return new JobParser();
+
+        }
+
+    }
+}
